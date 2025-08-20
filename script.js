c308e1b209eed71577f49ae3b0dffde7c6c3c6c4
Marcos Vinicius <marcosamaralwebdeveloper@outlook.com>
2025-08-20 19:29:49 -0300

    alteração na seção do responsivo.css e do script.js

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
 
-    accordionHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            const accordionItem = header.parentElement;
-            const accordionContent = accordionItem.querySelector('.accordion-content');
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        const accordionContent = accordionItem.querySelector('.accordion-content');
             
-            header.classList.toggle('active');
+        header.classList.toggle('active');
            
-            if (accordionContent.style.maxHeight) {
-                accordionContent.style.maxHeight = null;
-                accordionContent.style.padding = '0 20px';
-            } else {
-                accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-                accordionContent.style.padding = '15px 20px';
-            }
-        });
+        if (accordionContent.style.maxHeight) {
+            accordionContent.style.maxHeight = null;
+            accordionContent.style.padding = '0 20px';
+        } else {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+            accordionContent.style.padding = '15px 20px';
+          }
     });
+  });
 
-    const favoriteIcons = document.querySelectorAll('.favorite-icon');
-    const favoritesCountSpan = document.querySelector('.favorites-count');
-    let favoritesCount = 0;
+  const favoriteIcons = document.querySelectorAll('.favorite-icon');
+  const favoritesCountSpan = document.querySelector('.favorites-count');
+  let favoritesCount = 0;
 
-    favoriteIcons.forEach(icon => {
-        icon.addEventListener('click', () => {
-            if (icon.classList.contains('fas')) {
-                favoritesCount++;
-            } else {
-                favoritesCount--;
-            }
-            favoritesCountSpan.textContent = favoritesCount;
-        });
+  favoriteIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        if (icon.classList.contains('fas')) {
+            favoritesCount++;
+        } else {
+            favoritesCount--;
+        }
+      favoritesCountSpan.textContent = favoritesCount;
     });
+  });
 
-    const menuIcon = document.querySelector('.menu-icon');
-    const mainNav = document.querySelector('.main-nav');
+  const menuIcon = document.querySelector('.menu-icon');
+  const mainNav = document.querySelector('.main-nav');
     
-    menuIcon.addEventListener('click', () => {
+  menuIcon.addEventListener('click', () => {
     mainNav.classList.toggle('open');
-   });
+  });
 });
